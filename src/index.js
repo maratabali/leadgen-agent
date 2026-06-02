62fbddc8812d604f873f3add8d9ec803b7e430f9
JH IT Zone <deploy@jhitzone.com>
2026-06-02 10:00:45 +0000

    Fix frontend path to use public directory

--- src/index.js
+++ src/index.js
@@ -49,9 +49,11 @@ app.use('/api/settings', settingsRouter);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../public')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
+    if (!req.path.startsWith('/api/')) {
+      res.sendFile(path.join(__dirname, '../public/index.html'));
+    }
   });
 }
 
